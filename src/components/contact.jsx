9dfa1ba000c770ae51fc32c573c9e401b8dec31a
Alan Tsang <thfalan@gmail.com>
2024-04-10 22:08:07 +0800

    update page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import emailjs from "emailjs-com";
 import React from "react";
+import './contact.css';
 
 const initialState = {
   name: "",
@@ -9,6 +10,7 @@ const initialState = {
 };
 export const Contact = (props) => {
   const [{ name, email, message }, setState] = useState(initialState);
+  const [successMessage, setSuccessMessage] = useState("");
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -24,16 +26,17 @@ export const Contact = (props) => {
     {/* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */ }
     
     emailjs
-      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_PUBLIC_KEY")
-      .then(
-        (result) => {
-          console.log(result.text);
-          clearState();
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    .sendForm("service_a9zj5ym", "template_gwtyugv", e.target, "Spy0zMrAaFFxkdcxZ")
+    .then(
+      (result) => {
+        console.log(result.text);
+        setSuccessMessage("Message Sent!");
+        clearState();
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
   };
   return (
     <div>
@@ -44,10 +47,10 @@ export const Contact = (props) => {
               <div className="section-title">
                 <h2>Get In Touch</h2>
                 <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
+                  We will get back to you as soon as possible.
                 </p>
               </div>
+              {successMessage && <p className="success-message">{successMessage}</p>}
               <form name="sentMessage" validate onSubmit={handleSubmit}>
                 <div className="row">
                   <div className="col-md-6">
@@ -109,29 +112,6 @@ export const Contact = (props) => {
                 </span>{" "}
                 {props.data ? props.data.email : "loading"}
               </p>
-            </div>
-          </div>
-          <div className="col-md-12">
-            <div className="row">
-              <div className="social">
-                <ul>
-                  <li>
-                    <a href={props.data ? props.data.facebook : "/"}>
-                      <i className="fa fa-facebook"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.twitter : "/"}>
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.youtube : "/"}>
-                      <i className="fa fa-youtube"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
             </div>
           </div>
         </div>
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -7,8 +7,7 @@ export const Services = (props) => {
         <div className="section-title">
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            We provide Web3 development services using our proprietary blockchain technology.
           </p>
         </div>
         <div className="row">
